45c384cc23a4179590f76bb7b90a709fc621b8fb
Thomas Eizinger <thomas@eizinger.io>
2021-10-18 16:15:47 +1100

    fixup! Add timeouts to awaiting messages during contract setup

--- daemon/src/setup_contract.rs
+++ daemon/src/setup_contract.rs
@@ -179,7 +179,7 @@ pub async fn new(
     .context("Failed to send Msg2")?;
     let msg2 = stream
         .select_next_some()
-        .timeout(Duration::from_secs(30))
+        .timeout(Duration::from_secs(60))
         .await
         .context("Expected Msg2 within 30 seconds")?
         .try_into_msg2()
